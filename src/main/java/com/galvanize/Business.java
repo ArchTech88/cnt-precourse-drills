a1ac6be3250d0c01a046d2f2e52b42e66f06237b
Alex Cheng <alexcheng@Alexs-MacBook-Pro-3.local>
2017-02-04 02:27:01 -0800

    oops forgot to push

--- src/main/java/com/galvanize/Business.java
+++ src/main/java/com/galvanize/Business.java
@@ -1,6 +1,31 @@
 package com.galvanize;
 
-public class Business{
+import java.util.*;
 
-  
+
+public class Business implements Addressable{
+  private String name;
+  private ArrayList<Address> addresses = new ArrayList<Address>();
+
+  public Business(String name){
+    this.name = name;
+  }
+
+
+  public String getName(){
+    return name;
+  }
+
+  @Override
+  public List<Address> getAddresses(){
+    return addresses;
+  }
+
+  @Override
+  public void addAddress(Address newAddress){
+    addresses.add(newAddress);
+  }
+
+
+
 }
